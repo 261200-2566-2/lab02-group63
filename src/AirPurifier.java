11193f596141d7402b85adb2e007c7caf8780621
patthaveekarn <patthaveekarn2544@gmail.com>
2023-11-24 21:40:32 +0700

    Air & main

--- src/AirPurifier.java
+++ src/AirPurifier.java
@@ -11,33 +11,22 @@ public class AirPurifier {
         setSerialNo(serialNo);
         setLevel(level);
         getModel();
-        getSerialNo();
     }
     public void setModel(String model) {
         this.model = model;
     }
-    public String getModel() {
+    public void getModel() {
         AirPurifier.modelCount++; //count model
-        return model;
     }
     public void setSerialNo(String serialNo) {
         this.serialNo = serialNo;
     }
-    public String getSerialNo() {
-        return serialNo;
-    }
+
     public void setLevel(int level) {
         if (level >= 1 && level <= 200) {
             this.level = level;
         } else {
             throw new IllegalArgumentException("level between 1-200 !!!");
-        }
-    }
-    public void output() {
-        if (level >= 100 && power) {
-            System.out.println("--> The air purifier is currently filtering the air !");
-        } else {
-            System.out.println("--> The air purifier is not currently filtering the air !");
         }
     }
 
